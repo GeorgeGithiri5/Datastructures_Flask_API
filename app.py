87a1c5ae67182c7625cfd5fd31fef8e8842c87fc
GeorgeGithiri5 <georgegithiri002@gmail.com>
2021-06-09 10:44:25 +0300

    Stack And Queue

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from sqlalchemy.engine import Engine
 import linked_list
 import HashTable
 import binary_search_tree
+import CustomQueue
+import Stack
 
 import random
 
@@ -166,13 +168,47 @@ def get_one_blog_post(blog_post_id):
 
     return jsonify(post)
 
-@app.route('/blog_post/<blog_post_id>', methods=['GET'])
-def get_one_blog_post(blog_post_id):
-    pass
+@app.route('/blog_post/numeric_body', methods=['GET'])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all() 
+    q = CustomQueueAndStack.Queue()
 
-@app.route('/blog_post/<blog_post_id>', methods=['DELETE'])
+    for post in blog_posts:
+        q.enqueue(post)
+
+    return_list = []
+
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+        post.data.body = numeric_body
+
+        return_list.append(
+            {
+            "id":post.data.id,
+            "title":post.data.title,
+            "body":post.data.body,
+            "user_id":post.data.user_id
+            })
+
+
+@app.route('/blog_post/delete_last_10', methods=['DELETE'])
 def delete_blog_post(blog_post_id):
-    pass
+    blog_posts = BlogPost.query.all()
+
+    s = Stack.Stack()
+
+    for post in blog_posts:
+        s.push(post)
+
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete)
+        db.session.commit()
+
+    return jsonify({"message":"success"})
 
 if __name__ == "__main__":
     app.run(debug=True)
